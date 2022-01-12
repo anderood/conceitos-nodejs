e5578be9788a98c10f0c6713bf1b95204d8eb192
Anderson Santos <anderson.santos@eunerd.com.br>
2022-01-12 02:05:58 -0300

    Dando continuidade ao desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,19 +11,47 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const repo = {
+    id: uuid(),
+    title: title,
+    url: url,
+    techs: [...techs],
+    likes: 0
+  }
+
+  repositories.push(repo);
+  response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const items = {
+    id,
+    title,
+    url,
+    techs, 
+  }
+
+  const position = repositories.findIndex(item => item.id == id);
+  repositories[position] = items;
+  response.json(repositories[position]);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const position = repositories.findIndex(item => item.id == id)
+  repositories.splice(position);
+
+  response.json(repositories);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
